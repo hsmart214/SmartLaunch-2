b614e15b385ff213e37348bce2561f6ca4248bac
J. HOWARD SMART <hsmart@Js-MacBook-Pro.local>
2012-09-02 23:44:00 -0700

    Initial Commit

---
+++ Smart Launch/SLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  Smart Launch
+//
+//  Created by J. HOWARD SMART on 9/2/12.
+//  Copyright (c) 2012 J. HOWARD SMART. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
